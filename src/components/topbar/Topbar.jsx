9bfeda39857f15af1871d454343d24f68313762e
itsajay2195 <ajaykumar.95@yahoo.com>
2022-02-13 23:37:24 +0530

    designing the hamburger

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,30 @@
 import './topbar.scss'
+import { Mail, Person } from "@material-ui/icons"
 
 export default function Topbar() {
   return (
-    <div className='topbar'>
+    <div className='topbar active'>
       <div className='wrapper'>
         <div className="left">
           <a href="#intro" className="logo">Ajay</a>
-          <div className="itemContainer"></div>
+          <div className="itemContainer">
+            <Person className='icon'/> 
+              <span> +91 9710115165</span>
+          </div>
+
+          <div className="itemContainer">
+            <Mail className='icon'/>
+            <span> ajayrne@gmail.com</span>
+          </div>
+
         </div>
-        
+
         <div className="right">
-            right
+          <div className="hamburger">
+            <span className='line1'></span>
+            <span className='line2'></span>
+            <span className='line3'></span>
+          </div>
 
 
         </div>
