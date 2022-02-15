366a8e87146939e61750bb78e05c65c2a78b35b6
itsajay2195 <ajaykumar.95@yahoo.com>
2022-02-15 10:16:22 +0530

    completion of the intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,25 +1,37 @@
 import './intro.scss'
+import React, { useEffect, useRef } from 'react'
+import { init } from 'ityped'
 
 function Intro() {
+  const textRef  = useRef()
+  useEffect(() => {
+    init(textRef.current,{
+      showCursor:true,
+      backdelay:2000,
+      strings:["mobile apps ","web apps ","react-native ","react-js "],
+    })
+  }, [])
+
+
   return (
-    <div className='intro' id ="intro">
+    <div className='intro' id="intro">
       <div className="left">
-         <div className="imageContainer">
-           <img src="assets/profile.jpg" alt="profile" />
-         </div>
+        <div className="imageContainer">
+          <img src="assets/profile.jpg" alt="profile" />
+        </div>
 
       </div>
 
       <div className="right">
-          <div className="wrapper">
-            <h2>Hi There, I'm</h2>
-            <h1>Ajaykumar Rajasekarn</h1>
-            <h3>Freelance Developer <span></span></h3>
-          </div>
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Ajaykumar Rajasekaran</h1>
+          <h3>Freelance Developer <span ref={textRef}></span></h3>
+        </div>
 
-          <a href="#protfolio" >
-             <img src="assets/down.png" alt="down" />
-          </a>
+        <a href="#protfolio" >
+          <img src="assets/down.png" alt="down" />
+        </a>
       </div>
 
 
