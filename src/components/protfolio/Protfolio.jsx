49ba7d858ebfb994e9dca32057206afd7c14ed4c
itsajay2195 <ajaykumar.95@yahoo.com>
2022-02-15 20:41:14 +0530

    bootstraping the active tabs and it content

--- src/components/protfolio/Protfolio.jsx
+++ src/components/protfolio/Protfolio.jsx
@@ -2,8 +2,51 @@ import './protfolio.scss'
 
 function Protfolio() {
   return (
-    <div className='protfolio' id="protfolio">Protfolio</div>
+    <div className='protfolio' id="protfolio">
+      <ul>
+        <li className='active'>Featured</li>
+        <li>Mobile App</li>
+        <li>Web App</li>
+        <li>Featured</li>
+      </ul>
+
+     <div className='container'>
+        <div className='protfolio-item'>
+          <img src="assets/happyfox.jpg" alt="profile" />
+          <h3> Happfox Helpdesk app 2.0</h3>
+        </div>
+
+
+        <div className='protfolio-item'>
+          <img src="assets/happyfox.jpg" alt="profile" />
+          <h3> Happfox Helpdesk app 2.0</h3>
+        </div>
+
+
+        <div className='protfolio-item'>
+          <img src="assets/happyfox.jpg" alt="profile" />
+          <h3> Happfox Helpdesk app 2.0</h3>
+        </div>
+
+        <div className='protfolio-item'>
+          <img src="assets/happyfox.jpg" alt="profile" />
+          <h3> Happfox Helpdesk app 2.0</h3>
+        </div>
+
+
+        <div className='protfolio-item'>
+          <img src="assets/happyfox.jpg" alt="profile" />
+          <h3> Happfox Helpdesk app 2.0</h3>
+        </div>
+
+
+        <div className='protfolio-item'>
+          <img src="assets/happyfox.jpg" alt="profile" />
+          <h3> Happfox Helpdesk app 2.0</h3>
+        </div>
+     </div>
+    </div>
   )
-}
+} 
 
 export default Protfolio
